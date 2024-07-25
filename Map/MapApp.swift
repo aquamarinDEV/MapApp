fa9aa4557d062a2184db3e6bff7dd3bcb0109ce7
Gabi <marin.gabriel99@yahoo.com>
2024-07-26 01:02:50 +0300

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Gabriel Marin on 26.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
